764ef1c9dbbca2d9603a16d9a776209370246ad5
14f3v <khemphet.souvannaphasy@gmail.com>
2024-08-13 09:44:19 +0700

    apply es6 module testing

--- test/modules.js.es6/deleteBinding.test.es6.js
+++ test/modules.js.es6/deleteBinding.test.es6.js
@@ -3,20 +3,24 @@ import mssql from 'mssql';
 import MSSQLAdapter from "../../dist";
 import dbConfig from '../constant';
 
-async function insertionBinding() {
+async function deleteBinding() {
     const dbClientPools = new mssql.ConnectionPool(dbConfig);
 
-    test(`test insertion param binding`, async () => {
-        const bindingParams = new MSSQLAdapter(dbClientPools);
+    test(`test deletion param binding`, async () => {
 
-        try { if (!bindingParams.connectionPools.connected) await bindingParams.connectionPools.connect() }
+        try { if (!dbClientPools.connected) await dbClientPools.connect() }
         catch (exception) { console.log('exception', exception) }
 
-        const selectBinder = bindingParams.insertBinding({
+        const bindingParams = new MSSQLAdapter(dbClientPools);
+
+        const dataSets = {
             USER_MASTER_ID: '00001',
             STATUS: 'ACTIVE',
             REMARK: 'ແອັກທີບ'
-        });
+        };
+        const { REMARK, ...deletingDatasets } = dataSets;
+
+        const updateBinding = bindingParams.deleteBinding(deletingDatasets);
 
         const queryScript = `
         BEGIN
@@ -25,17 +29,21 @@ async function insertionBinding() {
             STATUS                  VARCHAR(150)            DEFAULT NULL,
             REMARK                  NVARCHAR(150)           DEFAULT NULL);
 
-            INSERT INTO @USER_MASTER ${selectBinder.column} VALUES ${selectBinder.value}
+            INSERT INTO @USER_MASTER (USER_MASTER_ID, STATUS, REMARK) VALUES 
+            ('${dataSets.USER_MASTER_ID}', '${dataSets.STATUS}', N'${dataSets.REMARK}');
+
+            DELETE @USER_MASTER 
+            WHERE ${updateBinding.value};
             SELECT * FROM @USER_MASTER;
 
         END
         `;
-
         const recordsets = await bindingParams.getRequestStatement.query(queryScript);
-        console.log('insertion recordsets', recordsets?.recordset);
+        console.log('recordset:', recordsets?.recordset);
         expect(recordsets.recordset.length).toBeInteger();
+        expect(recordsets.recordset.length).toBe(0);
     });
 
 }
 
-export default insertionBinding;
+export default deleteBinding;
